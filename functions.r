a1edc08b42bfd5e4c158116d5100f588d925bcbb
linxy29 <xinyi_lin@outlook.com>
2020-10-06 09:10:18 +0800

    simulation large data

--- functions.r
+++ functions.r
@@ -422,6 +422,130 @@ getPandTimeTestU = function(integratedSamples, sim_list){
   return(Res_df)
 }
 
+# function 7: function to get p-values and time(Fisher, speckle, dcats, diffcyt) for four clusters
+getPandTimeFSDD4 = function(integratedSamples, sim_list, batch_size = 1000){
+  time = rep(NA,4)
+  dfRes = data.frame(clusterRes = integratedSamples@active.ident, batch = integratedSamples$batch, condition = integratedSamples$condition) %>% 
+    tibble::rownames_to_column("cellID")
+  
+  ## Fisher's exact test
+  dfCount = dfRes %>% 
+    group_by(condition, clusterRes) %>% 
+    summarise(n = n()) %>% 
+    pivot_wider(names_from = "clusterRes", values_from = "n") %>% 
+    mutate(nonA = batch_size*6 - A,
+           nonB = batch_size*6 - B,
+           nonC = batch_size*6 - C,
+           nonD = batch_size*6 - D)
+  t1start = Sys.time()
+  fisher_pvals = rep(NA,4)
+  for (i in 1:4){
+    fisher_pvals[i] = fisher.test(dfCount[,c(i+1,i+5)])$p.value
+  }
+  time[1] = Sys.time() - t1start
+  fisherP = data.frame(cluster = c("A", "B", "C", "D"), fisher_pvals = fisher_pvals)
+  
+  ## speckle
+  t2start = Sys.time()
+  speckleRes = propeller(clusters = dfRes$clusterRes, sample = dfRes$batch, group = dfRes$condition)
+  time[2] = Sys.time() - t2start
+  #print(speckleRes)
+  speckleP = data.frame(cluster = speckleRes$BaselineProp.clusters, speckle_pvals = speckleRes$FDR)
+  
+  ## DCATS
+  celllabels_orig = sim_list$origLabels
+  conf.mat<-table(Idents(integratedSamples), celllabels_orig)
+  true.conf<-t(t(conf.mat)/apply(conf.mat,2,sum))
+  library(clue)
+  order = as.vector(solve_LSAP(true.conf, maximum = TRUE))
+  #print(true.conf)
+  condition = integratedSamples@meta.data$condition
+  condNor<-Idents(integratedSamples)[condition == "Normal"]
+  condMut<-Idents(integratedSamples)[condition == "Mutate"]
+  countNor = table(sim_list$batchNor, condNor)
+  countMut = table(sim_list$batchMut, condMut)
+  t3start = Sys.time()
+  dcatsResT = dcats_fit(countNor, countMut, true.conf[,order])  # true similarity matrix
+  time[3] = Sys.time() - t3start
+  dcatsResI = dcats_fit(countNor, countMut, diag(4))  # identity matrix
+  dcatsResU = dcats_fit(countNor, countMut, get_similarity_mat(4, 0.05)) # uniform matrix
+  graphs = integratedSamples@graphs$integrated_snn
+  labels = Idents(integratedSamples)
+  knn_mat = KNN_transition(graphs, labels)
+  order = colnames(countNor)
+  dcatsResK = dcats_fit(countNor, countMut, knn_mat[order, order])
+  #print(dcatsRes)
+  dcatsPT = data.frame(cluster = rownames(dcatsResT), dcats_pvalsT = dcatsResT$pvals)
+  dcatsPI = data.frame(cluster = rownames(dcatsResI), dcats_pvalsI = dcatsResI$pvals)
+  dcatsPU = data.frame(cluster = rownames(dcatsResU), dcats_pvalsU = dcatsResU$pvals)
+  dcatsPK = data.frame(cluster = rownames(dcatsResK), dcats_pvalsK = dcatsResK$pvals)
+  
+  ## diffcyt
+  # Function to create random data (one sample)
+  d_random <- function(n = 20*batch_size, mean = 0, sd = 1, ncol = 20, cofactor = 5) {
+    d <- sinh(matrix(rnorm(n, mean, sd), ncol = ncol)) * cofactor
+    colnames(d) <- paste0("marker", sprintf("%02d", 1:ncol))
+    d
+  }
+  # Create random data (without differential signal)
+  set.seed(123)
+  d_input <- list(
+    Nor1 = d_random(), 
+    Nor2 = d_random(), 
+    Nor3 = d_random(), 
+    Mut1 = d_random(),
+    Mut2 = d_random(),
+    Mut3 = d_random()
+  )
+  experiment_info <- data.frame(
+    sample_id = factor(c("Nor1", "Nor2", "Nor3", "Mut1", "Mut2", "Mut3")), 
+    group_id = factor(c(rep("Normal", 3), rep("Mutate", 3))), 
+    stringsAsFactors = FALSE
+  )
+  marker_info <- data.frame(
+    channel_name = paste0("channel", sprintf("%03d", 1:20)), 
+    marker_name = paste0("marker", sprintf("%02d", 1:20)), 
+    marker_class = factor(c(rep("type", 10), rep("state", 10)), 
+                          levels = c("type", "state", "none")), 
+    stringsAsFactors = FALSE
+  )
+  # Prepare data
+  d_se <- prepareData(d_input, experiment_info, marker_info)
+  # Transform data
+  d_se <- transformData(d_se)
+  # Generate clusters
+  d_se <- generateClusters(d_se)
+  # Replace with our simulated data info
+  d_se@elementMetadata$sample_id = integratedSamples$batch
+  d_se@elementMetadata$group_id = integratedSamples$condition
+  d_se@elementMetadata$cluster_id = integratedSamples@active.ident
+  # Calculate counts
+  d_counts <- calcCounts(d_se)
+  # Create design matrix
+  design <- createDesignMatrix(experiment_info, cols_design = "group_id")
+  # Create contrast matrix
+  contrast <- createContrast(c(0, 1))
+  # Test for differential abundance (DA) of clusters
+  t4start = Sys.time()
+  res_DA <- testDA_edgeR(d_counts, design, contrast)
+  time[4] = Sys.time() - t4start
+  diffcytP = topTable(res_DA, format_vals = TRUE) %>% 
+    as.data.frame() %>% 
+    rownames_to_column("cluster") %>% 
+    rename(diffcyt_pvals = p_adj) %>% 
+    select(cluster, diffcyt_pvals)
+  
+  ## results
+  Res_df = merge(dcatsPT, speckleP, by = "cluster") %>% 
+    merge(dcatsPI, by = "cluster") %>% 
+    merge(dcatsPU, by = "cluster") %>%
+    merge(dcatsPK, by = "cluster") %>%
+    merge(fisherP, by = "cluster") %>% 
+    merge(diffcytP, by = "cluster")
+  time_df = data.frame(methods = c("fisher", "sepckle", "dcats", "diffcyt"), time = time)
+  return(list(Res_df = Res_df, time_df = time_df))
+}
+
 # 0.1, 0.2 keep the same, 0.1->0.05, 0.2->0.1, 0.2->0.25, 0.2->0.3
 
 if(FALSE)
