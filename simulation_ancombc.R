c62b968cbb84f4de9fee7b1e61524c229fdde971
linxy29 <linxy29@connect.hku.hk>
2022-11-29 09:14:06 +0800

    add ancombc

--- simulation_ancombc.R
+++ simulation_ancombc.R
@@ -5,11 +5,39 @@ library(ANCOMBC)
 library(TreeSummarizedExperiment)
 
 ## load data
-load("/Users/linxy29/Documents/Data/DCATS/simulation/current/replicates2&2_K8_con100_splatter3000&3000para.RData")
-
-## ancombc test
-count_info = rbind(oper[[1]]$seurat_count$cond1, oper[[1]]$seurat_count$cond2)
-assay_data= count_info %>% 
-col_date()
+source("functionsV2.r")
 
 filenames = list.files("/Users/linxy29/Documents/Data/DCATS/simulation/current", full.names = TRUE)
+for (file in filenames[2:5]) {
+  load(file)
+  for (i in 1:length(oper)) {
+    if (is.na(oper[[i]])[1]) next
+    print(i)
+    count_info = rbind(oper[[i]]$seurat_count$cond1, oper[[i]]$seurat_count$cond2)
+    #count_info = rbind(oper[[i]]$seurat_count$cond1, oper[[i]]$seurat_count$cond2) %>%  mutate(condition = str_replace(condition, "Condition ", "cond"))
+    assay_data = count_info %>% 
+      #select(-condition) %>% 
+      as.matrix() %>% t()
+    col_data = data.frame(condition = rownames(count_info) %>% str_remove("s[1-9]"))
+    t9start = Sys.time()
+    ancombcRes = getANCOMBC(assay_data, col_data)
+    oper[[i]]$time = c(oper[[i]]$time, Sys.time() - t9start)
+    ## ancombc test with bias correction
+    dcats_bc_knn = dcats_bc(assay_data %>% t(), oper[[i]]$knn_matrix)
+    dcats_bc_true = dcats_bc(assay_data %>% t(), oper[[i]]$true_matrix)
+    dcats_bc_svm = dcats_bc(assay_data %>% t(), oper[[i]]$svm_matrix)
+    ancombc_knn = getANCOMBC(dcats_bc_knn %>% t(), col_data)
+    ancombc_true = getANCOMBC(dcats_bc_true %>% t(), col_data)
+    ancombc_svm = getANCOMBC(dcats_bc_svm %>% t(), col_data)
+    ## merge results
+    ancombcRes = ancombcRes %>% 
+      dplyr::rename(cluster = taxon) %>% 
+      dplyr::rename(ancombc_pvals = q_conditioncond2) %>% 
+      mutate(ancombc_knn_pvals = ancombc_knn$q_conditioncond2,
+             ancombc_true_pvals = ancombc_true$q_conditioncond2,
+             ancombc_svm_pvals = ancombc_svm$q_conditioncond2)
+    oper[[i]]$clusterDF = oper[[i]]$clusterDF %>% 
+      merge(ancombcRes)
+  }
+  save(oper, file = file)
+}
